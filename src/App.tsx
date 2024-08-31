dad59ec2af882d5549b9651c9345867350d4ef5c
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-08-31 13:22:08 +0100

    Initial commit for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [name, setName] = useState<string>('');
+
+  useEffect(() => {
+    getName();
+  }, []);
+
+  const getName = async () => {
+    return await Axios.get(`${process.env.REACT_APP_API_URL}/get-info`).then((res) => {
+      setName(res?.data?.data[0]?.name);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello {name}!</h1>
+        <p>This is a sample app for a deployment purpose</p>
       </header>
     </div>
   );
